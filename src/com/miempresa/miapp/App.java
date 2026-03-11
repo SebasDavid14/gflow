41f0d6cab254e6e1f592002772a6f602970f9664
SebasDavid14 <sebasdavid0211@gmail,com>
2026-03-10 19:03:04 -0500

    feature: agregar operaciones avanzadas

--- src/com/miempresa/miapp/App.java
+++ src/com/miempresa/miapp/App.java
@@ -1,4 +1,29 @@
 package com.miempresa.miapp;
 
 public class App {
+
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Suma: " + sumar(5,3));
+        System.out.println("Potencia: " + OperacionAvanzada.potencia(2,3));
+        System.out.println("Raiz: " + OperacionAvanzada.raizCuadrada(16));
+        System.out.println("Modulo: " + OperacionAvanzada.modulo(10,3));
+
+    }
 }
---
+++ src/com/miempresa/miapp/OperacionAvanzada.java
@@ -0,0 +1,15 @@
+package com.miempresa.miapp;
+
+public class OperacionAvanzada {
+    public static double potencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
+
+    public static double raizCuadrada(double numero) {
+        return Math.sqrt(numero);
+    }
+
+    public static double modulo(double a, double b) {
+        return a % b;
+    }
+}
